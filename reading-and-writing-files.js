a293c1716eb284e035ed3969deccb73e29ce4faf
Alexandre Calaça <alexcalaca@gmail.com>
2022-05-20 01:11:20 -0300

    Include reading functionality

--- reading-and-writing-files.js
+++ reading-and-writing-files.js
@@ -1,1 +1,8 @@
 
+const fs = require('fs');
+
+const variable = "Hey"
+console.log(variable)
+
+const textInput = fs.readFileSync('../boilerplate-expander/script.js', 'utf-8');
+console.log("The content is " + textInput);
