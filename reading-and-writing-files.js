d4bb594df5ae43042814c8f7183c0d7ff5dfcb05
Alexandre Calaça <alexcalaca@gmail.com>
2022-05-20 01:17:32 -0300

    Include file writing functionality

--- reading-and-writing-files.js
+++ reading-and-writing-files.js
@@ -6,3 +6,9 @@ console.log(variable)
 
 const textInput = fs.readFileSync('../boilerplate-expander/script.js', 'utf-8');
 console.log("The content is " + textInput);
+
+
+const textOutput = 'This is what I wrotee ${textInput}';
+fs.writeFileSync('../boilerplate-expander/new_file.js', textOutput);
+
+console.log('File written successfully')
