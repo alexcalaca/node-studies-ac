a743a2b85634344972fce84f196b11f408045bd1
Alexandre Calaça <alexcalaca@gmail.com>
2022-05-23 22:23:21 -0300

    Include root and overview path

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("index.js");
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer(( req, res) => {
+    
+    const pathName = req.url; 
+       
+        
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the overview path');
+    } else {      
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'Hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');        
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
