d968b67eba84105bbfa01d4f8af327b868c3294a
Alexandre Calaça <alexcalaca@gmail.com>
2022-05-24 09:08:44 -0300

    Refactor index javascript file

--- index.js
+++ index.js
@@ -2,23 +2,20 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const data = fs.readFileSync('./data.json', 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer(( req, res) => {
     
-    const pathName = req.url; 
-       
+    const pathName = req.url;        
         
     if(pathName === '/' || pathName === '/overview'){
         res.end('This is the overview path');
     } else if (pathName === '/product') {
         res.end('This is the product path');
-    } else if (pathName === '/api') {
-
-        fs.readFile('./data.json', 'utf-8', (err, data) => {
-            const productData = JSON.parse(data);
-            res.writeHead(200, { 'Content-type': 'application/json' });
-            res.end(data);            
-        });
-
+    } else if (pathName === '/api') {            
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(data);
     } else {      
         res.writeHead(404, {
             'Content-type': 'text/html',
