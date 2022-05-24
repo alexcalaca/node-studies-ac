cbe63aaa3123e8f6fc2d5742e88a3b6150b0174b
Alexandre Calaça <alexcalaca@gmail.com>
2022-05-24 08:58:06 -0300

    Add api reading process

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
@@ -11,7 +12,13 @@ const server = http.createServer(( req, res) => {
     } else if (pathName === '/product') {
         res.end('This is the product path');
     } else if (pathName === '/api') {
-        res.end('This is the api path');
+
+        fs.readFile('./data.json', 'utf-8', (err, data) => {
+            const productData = JSON.parse(data);
+            res.writeHead(200, { 'Content-type': 'application/json' });
+            res.end(data);            
+        });
+
     } else {      
         res.writeHead(404, {
             'Content-type': 'text/html',
