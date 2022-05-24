91e81d4a631a19f1c7097d05e9e23976ebd65f19
Alexandre Calaça <alexcalaca@gmail.com>
2022-05-23 22:05:34 -0300

    Update routing-node.js

--- routing-node.js
+++ routing-node.js
@@ -1,3 +1,12 @@
-
 const http = require('http');
 const url = require('url');
+
+const server = http.createServer(( req, res) => {
+    
+    const pathName = req.url; 
+       
+        
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the overview path');
+    } 
+  });
