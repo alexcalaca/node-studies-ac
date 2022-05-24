aa0471f8dd48920a7c572267c68f63fad2e6756c
Alexandre Calaça <alexcalaca@gmail.com>
2022-05-23 22:07:55 -0300

    Include 404 path

--- routing-node.js
+++ routing-node.js
@@ -12,5 +12,15 @@ const server = http.createServer(( req, res) => {
         res.end('This is the product path');
     } else if (pathName === '/api') {
         res.end('This is the api path');
-    } 
-  });
+    } else {      
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'Hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');        
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
